f372fb1018f82223ce71026b730faa248573f516
Veraces <Veraces@gmail.com>
2025-03-04 19:03:47 -0500

    Update janta_hides.lua

--- MMOCoreORB/bin/scripts/loot/items/npc/janta_hides.lua
+++ MMOCoreORB/bin/scripts/loot/items/npc/janta_hides.lua
@@ -6,8 +6,8 @@ janta_hides = {
 	customObjectName = "",
 	directObjectTemplate = "object/tangible/component/armor/armor_segment_enhancement_janta.iff",
 	craftingValues = {
-		{"armor_effectiveness",3,6,10},
-		{"useCount",1,11,0},
+		{"armor_effectiveness",10,15,20},
+		{"useCount",50,110,0},
 	},
 	customizationStringNames = {},
 	customizationValues = {}
